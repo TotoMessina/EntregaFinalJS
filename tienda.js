9c2a7ebcbf3629f88276897d400e60f7505b330c
TotoMessina <132374777+TotoMessina@users.noreply.github.com>
2024-05-27 20:57:14 -0400

    Todavia falta mejora estetica y arreglo del carrito

--- tienda.js
+++ tienda.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('/productos.json')
+    .then(response => response.json())
+    .then(productos => {
+      const productosContainer = document.getElementById('productos');
+      productos.forEach(producto => {
+        const productoDiv = document.createElement('div');
+        productoDiv.classList.add('producto');
+        
+        productoDiv.innerHTML = `
+          <h2>${producto.nombre}</h2>
+          <div class="image-container">
+            <img src="${producto.imagen}" alt="fotoproducto">
+          </div>
+          <p>Precio: $${producto.precio}</p>
+          <button class="agregar-carrito" onclick="agregarAlCarrito('${producto.nombre}', ${producto.precio})">Agregar al carrito</button>
+        `;
+        
+        productosContainer.appendChild(productoDiv);
+      });
+    })
+    .catch(error => console.error('Error al cargar los productos:', error));
+});
+
 let carrito = [];
 
 function agregarAlCarrito(producto, precio) {
@@ -11,6 +35,7 @@ function mostrarCantidadCarrito() {
 }
 
 function mostrarCarrito() {
+  console.log('Carrito clicado');
   const productosCarrito = document.getElementById('productos-carrito');
   productosCarrito.innerHTML = '';
   if (carrito.length === 0) {
@@ -18,9 +43,10 @@ function mostrarCarrito() {
   } else {
     carrito.forEach(item => {
       const producto = document.createElement('div');
-      producto.textContent = `${item.producto} - Precio: $${item.precio}`;
+      producto.textContent = `${item.nombre} - Precio: $${item.precio}`;
       productosCarrito.appendChild(producto);
     });
   }
   productosCarrito.classList.toggle('oculto');
+  console.log('productos-carrito:', productosCarrito.classList);
 }
